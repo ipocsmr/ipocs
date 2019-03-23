34fcd59f1a1d88a765fefb3317986838c71c030f
Fredrik Elestedt <fredrik@elestedt.com>
2019-03-23 18:43:32 +0100

    Return of the WebSocket

--- src/esp8266/http.h
+++ src/esp8266/http.h
@@ -2,7 +2,9 @@
 #define HTTPHANDLER_H
 
 #include <stdint.h>
+#include <WebSockets.h>
 
+class WebSocketsServer;
 class ESP8266WebServer;
 class ESP8266HTTPUpdateServer;
 class String;
@@ -25,8 +27,10 @@ namespace esp {
       private:
         ~Http();
         Http();
+        WebSocketsServer* webSocket;
         ESP8266WebServer* server;
         ESP8266HTTPUpdateServer* updateServer;
+        void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
     };
 }
 
